3c376bb01bceb30b8e0e84e7e2bede6244cdf4fe
THEjacob1000 <jacob.s.sanderson@gmail.com>
2023-12-06 13:57:04 +1100

    refactor: optimise build for deployment

--- src/actions/delete-board/index.ts
+++ src/actions/delete-board/index.ts
@@ -10,9 +10,11 @@ import { redirect } from "next/navigation";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { decreaseAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
+  const isPro = await checkSubscription();
 
   if (!userId || !orgId) {
     throw new Error("Unauthorized");
@@ -29,7 +31,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    await decreaseAvailableCount();
+    if (!isPro) {
+      await decreaseAvailableCount();
+    }
 
     await createAuditLog({
       entityTitle: board.title,
