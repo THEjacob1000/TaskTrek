b4c318ffb16bf38b0c9aa87468dda45b8f72b0ff
THEjacob1000 <jacob.s.sanderson@gmail.com>
2023-12-06 12:09:01 +1100

    feat: added pro popup when breaching free plan

--- src/components/providers/ModalProvider.tsx
+++ src/components/providers/ModalProvider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import CardModal from "../modals/card-modal";
+import ProModel from "../modals/ProModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +18,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CardModal />
+      <ProModel />
     </>
   );
 };
